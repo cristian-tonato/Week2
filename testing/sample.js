7b0a276e1de26f23fcac3c539e063888364c7726
Cristian <cristianotona@gmail.com>
2022-10-13 10:01:20 +0200

    test/examples

--- testing/sample.js
+++ testing/sample.js
@@ -1,4 +1,10 @@
 export function add(a,b){
-    return a+b
+    if(a===0){
+        return "error";
+    }
+    return a+b;
 }
 
+export function subtraction (a,b){
+    return a-b;
+}
--- testing/sample.test.js
+++ testing/sample.test.js
@@ -1,4 +1,6 @@
 import { add} from "./sample.js";
+import {subtraction } from "./sample.js";
+
 
 describe("testing add", ()=>{
 
@@ -7,8 +9,26 @@ describe("testing add", ()=>{
         let b=2;
         expect(add(a,b)).toBe(4)
 })
+    test ("if a =2 and b = 2 add should be 4", ()=>{
+        let a =2;
+        let b=2;
+        expect(add(a,b)).toBeLessThan(5);
+    });
+
+    test(`if a = 0, return should be "Error"`, ()=>{
+        let a = 0;
+        let b = 2;
+        expect(add(a,b)).toBe(`Error`);
+    })
 
 });
 
+describe ("testing subtraction", () =>{
+     test('if a =2 and b=2 add should be 0', () => {
+         let a = 2;
+         let b = 2;
+         expect(subtraction(a, b)).toBe(0);
+     });
+})
 
 
